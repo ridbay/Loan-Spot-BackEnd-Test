72bd0773d5a79722ba13b216ae6da58cb93bedfb
Ridwan Balogun <ridwan.balogun@mypatricia.co>
2022-06-18 16:13:49 +0100

    heroku deployment

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.all("*", (request, response) => {
 
 // set port, listen for requests
 const PORT = 6000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
